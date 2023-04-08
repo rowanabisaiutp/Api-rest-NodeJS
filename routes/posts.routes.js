175ca1c6579009b7b29dc4e562f66c76e999f97d
Rowan Abisai <107498038+rowanabisaiutp@users.noreply.github.com>
2023-04-07 21:17:10 -0600

    Update posts.routes.js

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -44,7 +44,7 @@ router.get("/", postsController.getAll)
 router.get('/:id', postsController.getById)
 router.post('/', postsController.create)
 router.put("/:id", postsController.update)
-
+router.delete("/:id", postsController.delete)
 //impoertar modulo
 
 module.exports = router
